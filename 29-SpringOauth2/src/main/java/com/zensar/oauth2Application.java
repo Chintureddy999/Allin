7af43ce4c3267e93d52c5e0aa3dc2d5ec82eb404
Neelakanteswar Reddy <95019154+Chintureddy999@users.noreply.github.com>
2022-06-15 11:08:55 +0530

    Add files via upload

---
+++ 29-SpringOauth2/src/main/java/com/zensar/oauth2Application.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(oauth2Application.class, args);
+	}
+
+}
